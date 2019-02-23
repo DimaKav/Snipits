d6a8d3d2852c89f6c4ec0e5e193302eb50708ab1
Dmitriy <dkav@live.com>
2019-02-23 16:17:22 -0500

    Update Gaussian_model.py

--- Gaussian_model.py
+++ Gaussian_model.py
@@ -1,4 +1,12 @@
+# Define gaussian model function
 def gaussian_model(x, mu, sigma):
     coeff_part = 1/(np.sqrt(2 * np.pi * sigma**2))
     exp_part = np.exp( - (x - mu)**2 / (2 * sigma**2) )
     return coeff_part*exp_part
+
+# Compute sample statistics
+mean = np.mean(sample)
+stdev = np.std(sample)
+
+# Model the population using sample statistics
+population_model = gaussian(sample, mu=mean, sigma=stdev)
