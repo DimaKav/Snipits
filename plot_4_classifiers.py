85d9a80bbe0aa6de15a9b6557586840eff373c15
Dmitriy <dkav@live.com>
2019-02-23 21:16:38 -0500

    Create plot_4_classifiers.py

---
+++ plot_4_classifiers.py
@@ -0,0 +1,25 @@
+def plot_4_classifiers(X, y, clfs):
+
+    # Set-up 2x2 grid for plotting.
+    fig, sub = plt.subplots(2, 2)
+    plt.subplots_adjust(wspace=0.2, hspace=0.2)
+
+    for clf, ax, title in zip(clfs, sub.flatten(), ("(1)", "(2)", "(3)", "(4)")):
+        # clf.fit(X, y)
+        plot_classifier(X, y, clf, ax, ticks=True)
+        ax.set_title(title)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
+
+# Define the classifiers
+classifiers = [LogisticRegression(), LinearSVC(), SVC(), KNeighborsClassifier()]
+
+# Fit the classifiers
+for c in classifiers:
+    c.fit(X, y)
+
+# Plot the classifiers
+plot_4_classifiers(X, y, classifiers)
+plt.show()
